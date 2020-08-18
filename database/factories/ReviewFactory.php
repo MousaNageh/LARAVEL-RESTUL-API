5d759833a6afcd3405fc4510ef5e1a0e1803590c
mousa nageh <200moussa200@gmail.com>
2020-08-18 04:10:51 +0200

    make fake data for products and reviews

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,11 +2,15 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Product;
 use App\Review;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        "customer"=>$faker->name() , 
+        "review"=>$faker->sentence(200) , 
+        "star"=>$faker->numberBetween(0,5) , 
+        "product_id"=>$faker->numberBetween(1,2000) 
     ];
 });
